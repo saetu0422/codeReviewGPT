7c01385cd2140780ee97b4696397c3bdf3c1be07
Junsoi <83137169+saetu0422@users.noreply.github.com>
2024-08-23 16:43:54 +0900

    Update srcsearch.py

--- srcsearch.py
+++ srcsearch.py
@@ -1,13 +1,13 @@
 import glob
 
 def fileAll():
-    file_list = glob.glob('./AI-Scientist-main/AI-Scientist-main/**', recursive=True)
+    file_list = glob.glob('../../AI-Scientist-main/AI-Scientist-main/**', recursive=True)
     return file_list
 
 def filePy():
-    file_list = glob.glob('./AI-Scientist-main/AI-Scientist-main/**/*.py', recursive=True)
+    file_list = glob.glob('../../AI-Scientist-main/AI-Scientist-main/**/*.py', recursive=True)
     return file_list
 
 def fileRQ():
-    file_list = glob.glob('./AI-Scientist-main/AI-Scientist-main/*.txt')
+    file_list = glob.glob('../../AI-Scientist-main/AI-Scientist-main/*.txt')
     return file_list
